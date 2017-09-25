62a7260ffcb03c4f218251027e9dc926fe8c45b5
Saurav <inallib@users.noreply.github.com>
2017-09-25 14:09:08 +0530

    Add files via upload

---
+++ ClinikoWeb/src/main/java/com/clinikoweb/ClinikoWebApplication.java
@@ -0,0 +1,12 @@
+package com.clinikoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinikoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinikoWebApplication.class, args);
+	}
+}
